4a25e4656583f8ae560aac35b3a18bf3f000e0cf
Gabriel Tan <gabriel.v.tan@gmail.com>
2018-08-27 18:31:13 -0400

    Final Dapp

---
+++ Linnia-Hackathon-Final/README.md
@@ -0,0 +1,25 @@
+# React + React Router Boilerplate
+Latest React and React Router ready to go. Includes Babel plugin for `async` feature, and webpack loaders for css modules 🎨.
+
+### Specs
+* React 16
+* React Router 4
+* Babel 6
+
+Test
+
+
+To install and run
+
+$ git clone https://github.com/MarkGeeRomano/linnia-hackathon.git
+
+$ cd linnia-hackathon
+
+$ npm install
+
+$ npm start
+
+Open a browser and run http://localhost:8080/
+
+
+
---
+++ Linnia-Hackathon-Final/src/Header.js
@@ -0,0 +1,18 @@
+import React from "react";
+ 
+import { Link } from "react-router-dom";
+ 
+const Header = (props) => {
+  return (
+   
+// combined View & Add Recrods and View & Add Permissions 
+ <div>
+ <Link to='/'><span>View/Add Records</span></Link>&nbsp;&nbsp; &nbsp;&nbsp; 
+ <Link to='/permission'><span>View/Add Permissions</span></Link>&nbsp;&nbsp; &nbsp;&nbsp; 
+ <Link to='/thirdparty'><span>Third Party</span></Link>&nbsp;&nbsp; &nbsp;&nbsp;    
+</div>
+ 
+  );
+};
+
+export default Header;
